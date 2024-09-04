a71110e496f2460ec2b24c5d55fa3a149b412767
Your Name <you@example.com>
2024-09-04 14:11:55 +0900

    Addition

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(){
-    std::cout << "__cplusplus: " << __cplusplus << std::endl; // 추가
+    std::cout << "__cplusplus: " << __cplusplus << std::endl; // 추가했다! 
     std::cout << "Hello World" << std::endl;
 }
